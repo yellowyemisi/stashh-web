16fd5af2403624b093355ecd24b12607ffd5cb2b
“Kate <111000569+yellowyemisi@users.noreply.github.com>
2026-02-13 21:15:59 +0100

    Chore: enable video recording for all test runs

--- playwright.config.js
+++ playwright.config.js
@@ -30,7 +30,10 @@ export default defineConfig({
     // baseURL: 'http://localhost:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    video: "on",
+
+    // size of video
+    viewport: { width: 1280, height: 720 },
   },
 
   /* Configure projects for major browsers */
